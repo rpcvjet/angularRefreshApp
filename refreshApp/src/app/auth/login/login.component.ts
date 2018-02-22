73fb51fd96b843b8c199ba4c417bdb8dc49e52f8
Kenneth Edwards (Cortac Group Inc) <v-keed@microsoft.com>
2018-02-22 15:21:42 -0800

    finished section3 no errors

--- refreshApp/src/app/auth/login/login.component.ts
+++ refreshApp/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from "@angular/forms";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,4 +12,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmit(form:NgForm) {
+    console.log(form)
+  }
 }
